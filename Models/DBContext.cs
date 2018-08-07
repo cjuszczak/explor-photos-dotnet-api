ad321e51b17f20bfe1f84c811f62b100c9af75f3
Charles Juszczak <charles.juszczak@gmail.com>
2018-08-06 17:58:39 -0700

    Add EF Core Model
    -Added Entity Framework Core model

--- Models/DBContext.cs
+++ Models/DBContext.cs
@@ -4,6 +4,8 @@ namespace explorphotosdotnetapi.Models
 {
     public class ExplorContext : DbContext
     {
+        public DbSet<Photo> Photos { get; set; }
+
         public ExplorContext(DbContextOptions<ExplorContext> options) 
             : base(options)
         {
---
+++ Models/Photo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace explorphotosdotnetapi.Models
+{
+    public class Photo
+    {
+        public int photoId { get; set; }
+        public string lat { get; set; }
+        public string lng { get; set; }
+        public string fileName { get; set; }
+        public string camera { get; set; }
+        public string lens { get; set; }
+        public DateTime uploaded { get; set; }
+    }
+}
